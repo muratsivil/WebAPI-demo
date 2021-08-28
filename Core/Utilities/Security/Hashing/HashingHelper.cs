f7659bf58c5487101bdfcdeb86a82bd08894372c
murat.sivil <murat.sivil@outlook.com>
2021-08-28 23:19:23 +0300

    Security folder created. Encryption, Hashing, JWT

---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        // Bir passwordun hashını oluşturuyor
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key; // Her kullanıcı için burada bir hash oluşturuyoruz.
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            }
+        }
+
+        //Sonradan giriş yapan userın verdiği passwordun bizim veri kaynağındaki hash ile ilgili salta göre eşleşip eşleşmediğini kontrol ediyor. 
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i]!=passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
