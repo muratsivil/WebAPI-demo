f67ddfc4a41d8f5618f226135855775a8de83f64
murat.sivil <murat.sivil@outlook.com>
2021-09-08 03:02:35 +0300

    CategoriesController created for API

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -30,7 +30,8 @@ namespace WebAPI
             //Ioc Autofac olmadan yaptýðýmýz þekli
             //services.AddSingleton<IProductService, ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
-            
+            services.AddCors();
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -60,6 +61,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
